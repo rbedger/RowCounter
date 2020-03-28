e11924de03e72e1acf4a2b2cae96403bccfaa2c2
Robert Edger <rbedger@gmail.com>
2020-03-28 15:41:51 -0500

    add basic bottom view linked to app settings

--- app/src/main/java/com/robenheimer/rowcounter/MainActivity.java
+++ app/src/main/java/com/robenheimer/rowcounter/MainActivity.java
@@ -1,19 +1,59 @@
 package com.robenheimer.rowcounter;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.robenheimer.RowCounter.MESSAGE";
+
+    private MainActivity that = this;
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        toolbar.setTitle("Robert");
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+
+            switch (menuItem.getItemId()){
+                case R.id.navigation_settings:
+                    loadFragment(new SettingsActivity.SettingsFragment());
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 
     /** Called when the user taps the send button */
